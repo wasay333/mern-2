104da4b2f855059cde1f6d2c70ac7734556a664b
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-22 15:01:52 +0500

    create pages and apply react router

--- cleint/src/App.jsx
+++ cleint/src/App.jsx
@@ -1,7 +1,26 @@
-export default function App() {
+import {BrowserRouter, Routes ,Route} from "react-router-dom"
+import React from 'react'
+import About from "./pages/About.jsx"
+import SignUp from "./pages/SignUp.jsx"
+import SignIn from "./pages/SignIn.jsx"
+import Home from "./pages/Home.jsx"
+import Dashboard from "./pages/Dashboard.jsx"
+import Project from "./pages/Project.jsx"
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+<Routes>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/sign-in" element={<SignIn/>}/>
+  <Route path="/sign-up" element={<SignUp/>}/>
+  <Route path="/project" element={<Project/>}/>
+  <Route path="/dashboard" element={<Dashboard/>}/>
+  <Route path="/about" element={<About/>}/>
+
+</Routes>
+    </BrowserRouter>
   )
 }
+
+export default App
