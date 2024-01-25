fb250b7ab806b26dae971dc95014731001d0fa48
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-25 14:27:50 +0500

    connected to database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("Pinged your deployment. You successfully connected to MongoDB!")
+}
+).catch((err)=>{
+    console.error(err)
+}
+)
 app.listen(3000, ()=>{
 console.log("server is running on port 3000")
 }
