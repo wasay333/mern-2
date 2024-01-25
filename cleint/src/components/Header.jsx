eda3ddd5737a8c56afc71996815ae03b7a9415f9
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-25 14:05:27 +0500

    create a backend server

--- cleint/src/components/Header.jsx
+++ cleint/src/components/Header.jsx
@@ -8,14 +8,16 @@ export default function Header() {
   return (
     <Navbar className='border-b-2'>
       <Link to='/'className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>wasay's</span>Blog</Link>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>wasay's</span>
+  Blog</Link>
       <form>
 <TextInput
-type='text'placeholder='Search...'rightIcon={AiOutlineSearch}className='hidden lg:inline'/></form>
+type='text'placeholder='Search...'rightIcon={AiOutlineSearch}className='hidden lg:inline'/>
+</form>
 <Button className='w-12 h-10 lg:hidden' color='gray' pill><AiOutlineSearch /></Button>
       <div className='flex gap-2 md:order-2'>
         <Button className='w-12 h-10 hidden sm:inline' color='gray' pill><FaMoon/></Button>
-        <Link to='/sign-in'><Button gradientDuoTone='purpleToBlue'>Sign In</Button></Link>
+        <Link to='/sign-in'><Button gradientDuoTone='purpleToBlue' outline>Sign In</Button></Link>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
